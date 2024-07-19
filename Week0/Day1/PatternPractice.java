0947539925af6a9fe9c1244be31cb6a0b656b9ae
Ashish Mishra <116095670+ashishmi8113@users.noreply.github.com>
2024-07-19 10:38:15 +0530

    Update PatternPractice.java

--- Week0/Day1/PatternPractice.java
+++ Week0/Day1/PatternPractice.java
@@ -684,24 +684,5 @@ public class PatternPractice {
     }
     
     public static void main(String[] args){
-        int n=5;
-        for (int i = 1; i <=n; i++) {
-            for (int k = 1; k <=n-i; k++) {
-                System.out.print("  ");
-            }
-            for (int j = 1; j <=i; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-        for (int i = n-1; i >=1; i--) {
-            for (int k = 1; k <=n-i; k++) {
-                System.out.print("  ");
-            }
-            for (int j = 1; j <=i; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-    }
+        pattern33(10);
 }
