5598a63fb1e7742f74d232eb5d24a42327711b18
Ashish Mishra <iam.ashishmi@gmail.com>
2024-07-28 22:31:28 +0530

    Sort Prac

---
+++ Week2/Assignment_2Sol/AssignMent_2Sol.java
@@ -0,0 +1,7 @@
+package Week2.Assignment_2Sol;
+
+public class AssignMent_2Sol {
+    public static void main(String[] args) {
+        System.out.println("Ashish Mishra");
+    }
+}
---
+++ Week2/Practice/SortingPractice.java
@@ -0,0 +1,74 @@
+package Week2.Practice;
+
+import java.util.Arrays;
+
+public class SortingPractice {
+    public static void disp(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+
+
+    public static void bubbleSort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length-1; j++) {
+                if (arr[j]>arr[j+1]) {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+
+
+
+
+
+    public static void selectionSort(int[] arr){
+        for (int i = 0; i < arr.length-1; i++) {
+            // for (int j = i; j < arr.length; j++) {
+                int idx=minimumValue(arr, i);
+                int temp=arr[idx];
+                arr[idx]=arr[i];
+                arr[i]=temp;
+            // }
+        }
+    }
+    public static int minimumValue(int[] arr, int a){
+        int mini=a;
+        for (int i = a+1; i < arr.length; i++) {
+            if(arr[mini]>arr[i]){
+                mini=i;
+            }
+        }
+        return mini;
+    }
+
+
+
+
+    public static void insertionSort(int[] arr){
+        for (int i = 1; i < arr.length; i++) {
+            int key=arr[i];
+            int j=i-1;
+            while(j>=0 && arr[j]>key){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr={5,4,3,2,1};
+        System.out.println("Ashish Mishra");
+        // bubbleSort(arr);
+        // selectionSort(arr);
+        // insertionSort(arr);
+        Arrays.sort(arr);
+        System.out.println(Arrays.binarySearch(arr,5));
+        disp(arr);
+    }
+}
