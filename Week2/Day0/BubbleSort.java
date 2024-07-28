10f0c3fae01737a523544bd58db1025af14c3577
Ashish Mishra <iam.ashishmi@gmail.com>
2024-07-28 09:28:08 +0530

    Update Bubble

--- Week2/Day0/BubbleSort.java
+++ Week2/Day0/BubbleSort.java
@@ -3,12 +3,17 @@ package Week2.Day0;
 public class BubbleSort {
     public static void bubbleSort(int[] arr){
         for (int turn = 1; turn < arr.length; turn++) {
+            int flag=0;
             for (int i = 0; i < arr.length-turn; i++) {
                 if(arr[i]>arr[i+1]){
                     int temp=arr[i];
                     arr[i]=arr[i+1];
                     arr[i+1]=temp;
+                    flag=1;
                 }
+            }
+            if(flag==0){
+                break;
             }
         }
         for (int i = 0; i < arr.length; i++) {
